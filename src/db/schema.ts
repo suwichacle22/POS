ec79725b3e41a834cddc86b266d382b4773691e0
suwichacle22 <suwichacle22@gmail.com>
2026-01-03 23:04:29 +0700

    review schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,8 +8,18 @@ import {
   uuid,
 } from "drizzle-orm/pg-core";
 
+export const transactionStatusEnum = pgEnum("transaction_status", [
+  "pending",
+  "submitted",
+]);
+export const customerTypeEnum = pgEnum("customer_type", ["farmer", "employee"]);
+export const productSplitTypeEnum = pgEnum("product_split_type_enum", [
+  "percentage",
+  "per_kg",
+]);
+
 export const farmers = pgTable("farmers", {
-  farmerId: uuid("farmer_id").primaryKey(),
+  farmerId: uuid("farmer_id").primaryKey().defaultRandom(),
   displayName: text("display_name").notNull(),
   phone: text("phone"),
   createdAt: timestamp("created_at").defaultNow(),
@@ -18,8 +28,8 @@ export const farmers = pgTable("farmers", {
     .$onUpdate(() => new Date()),
 });
 
-export const employees = pgTable("employee", {
-  employeeId: uuid("employee_id").primaryKey(),
+export const employees = pgTable("employees", {
+  employeeId: uuid("employee_id").primaryKey().defaultRandom(),
   farmerId: uuid("farmer_id")
     .notNull()
     .references(() => farmers.farmerId),
@@ -32,14 +42,9 @@ export const employees = pgTable("employee", {
     .$onUpdate(() => new Date()),
 });
 
-export const productSplitTypeEnum = pgEnum("product_split_type_enum", [
-  "percentage",
-  "per_kg",
-]);
-
-export const products = pgTable("product", {
-  productId: uuid("product_id").primaryKey(),
-  product_name: text("product_name").notNull(),
+export const products = pgTable("products", {
+  productId: uuid("product_id").primaryKey().defaultRandom(),
+  productName: text("product_name").notNull(),
   defaultSplitType: productSplitTypeEnum("default_split_type").notNull(),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at")
@@ -47,38 +52,51 @@ export const products = pgTable("product", {
     .$onUpdate(() => new Date()),
 });
 
-export const product_prices = pgTable("product_price", {
-  productPriceId: uuid("product_price_id").primaryKey(),
-  productId: uuid("product_id").references(() => products.productId),
-  price: numeric("price").notNull(),
+export const productPrices = pgTable("product_prices", {
+  productPriceId: uuid("product_price_id").primaryKey().defaultRandom(),
+  productId: uuid("product_id")
+    .notNull()
+    .references(() => products.productId),
+  price: numeric("price", { precision: 10, scale: 2 }).notNull(),
   createdAt: timestamp("created_at").defaultNow(),
 });
 
 export const splitDefaults = pgTable("split_defaults", {
-  splitDefaultId: uuid("split_default_id").primaryKey(),
-  employeeId: uuid("employee_id").references(() => employees.employeeId),
-  productId: uuid("product_id").references(() => products.productId),
+  splitDefaultId: uuid("split_default_id").primaryKey().defaultRandom(),
+  employeeId: uuid("employee_id")
+    .notNull()
+    .references(() => employees.employeeId),
+  productId: uuid("product_id")
+    .notNull()
+    .references(() => products.productId),
   splitType: productSplitTypeEnum("split_type").notNull(),
-  farmerSplitRatio: numeric("farmer_split_ratio"),
-  employeeSplitRatio: numeric("employee_split_ratio"),
-  harvestRate: numeric("harvest_rate"),
-  transportationFee: numeric("transportation_fee"),
+  farmerSplitRatio: numeric("farmer_split_ratio", {
+    precision: 3,
+    scale: 2,
+  }),
+  employeeSplitRatio: numeric("employee_split_ratio", {
+    precision: 3,
+    scale: 2,
+  }),
+  harvestRate: numeric("harvest_rate", { precision: 3, scale: 2 }),
+  promotionTo: customerTypeEnum("promotion_to"),
+  transportationFee: numeric("transportation_fee", {
+    precision: 3,
+    scale: 2,
+  }),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at")
     .defaultNow()
     .$onUpdate(() => new Date()),
 });
 
-export const transactionStatusEnum = pgEnum("transaction_status", [
-  "pending",
-  "submitted",
-]);
-
 export const transactionGroups = pgTable("transaction_groups", {
-  transactionGroupId: uuid("transaction_group_id").primaryKey(),
-  farmerId: uuid("farmer_id").references(() => farmers.farmerId),
+  transactionGroupId: uuid("transaction_group_id").primaryKey().defaultRandom(),
+  farmerId: uuid("farmer_id")
+    .notNull()
+    .references(() => farmers.farmerId),
   groupName: text("group_name"),
-  status: transactionStatusEnum("status").notNull(),
+  status: transactionStatusEnum("status").notNull().default("pending"),
   printCount: integer("print_count").notNull().default(0),
   createdAt: timestamp("created_at").defaultNow(),
   submittedAt: timestamp("submitted_at"),
@@ -87,22 +105,46 @@ export const transactionGroups = pgTable("transaction_groups", {
     .$onUpdate(() => new Date()),
 });
 
-export const transactions = pgTable("transactions", {
-  transactionId: uuid("transaction_id").primaryKey(),
-  transactionGroupId: uuid("transaction_group_id").references(
-    () => transactionGroups.transactionGroupId,
-  ),
-  employeeId: uuid("employee_id").references(() => employees.employeeId),
+export const transactionLines = pgTable("transaction_lines", {
+  transactionId: uuid("transaction_id").primaryKey().defaultRandom(),
+  transactionGroupId: uuid("transaction_group_id")
+    .notNull()
+    .references(() => transactionGroups.transactionGroupId),
+  employeeId: uuid("employee_id")
+    .notNull()
+    .references(() => employees.employeeId),
   productId: uuid("product_id").references(() => products.productId),
-  weight: numeric("weight").notNull(),
-  price: numeric("price").notNull(),
-  splitType: productSplitTypeEnum("split_type").notNull(),
-  farmerRatio: numeric("farmer_ratio"),
-  employeeRatio: numeric("employee_ratio"),
-  harvestRate: numeric("harvest_rate"),
-  transportationFee: numeric("transportation_fee"),
+  weight: numeric("weight", {
+    precision: 10,
+    scale: 2,
+  }).notNull(),
+  price: numeric("price", {
+    precision: 10,
+    scale: 2,
+  }).notNull(),
+  splitType: productSplitTypeEnum("split_type").notNull().default("percentage"),
+  farmerRatio: numeric("farmer_ratio", {
+    precision: 3,
+    scale: 2,
+  }),
+  employeeRatio: numeric("employee_ratio", {
+    precision: 3,
+    scale: 2,
+  }),
+  harvestRate: numeric("harvest_rate", {
+    precision: 3,
+    scale: 2,
+  }),
+  transportationFee: numeric("transportation_fee", {
+    precision: 3,
+    scale: 2,
+  }),
   carLicense: text("car_license"),
-  promotionRate: numeric("promotion_rate"),
+  promotionRate: numeric("promotion_rate", {
+    precision: 3,
+    scale: 2,
+  }),
+  promotionTo: customerTypeEnum("promotion_to").default("employee"),
   createdAt: timestamp("created_at").defaultNow(),
   submittedAt: timestamp("submitted_at"),
   updatedAt: timestamp("updated_at")
